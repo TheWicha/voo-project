f4a13df63120cc48d486ae893cc7fc27db988270
TheWicha <kontakt.danielwisniewski@gmail.com>
2019-07-20 11:49:49 +0200

    Add styles for mobile devices

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -7,12 +7,12 @@ function About(props) {
     <section className='about'>
       <div className='about-us'>
         <h1>Our Story</h1>
-        <img src={img} alt='Voo logo'/>
+        <img src={img} alt='Voo logo' />
         <p>
           Voo is a brick & mortar, quick service restaurant inspired by street vendors,
           carts & trucks from across the country & the globe. Check out either of our locations
           for a quick bite or hang at bar for some of the best nightlife in Poland.
-      </p>
+        </p>
       </div>
     </section>
   )
