55dc9d63a91c9775206749261fc4fae09f8d0619
TheWicha <kontakt.danielwisniewski@gmail.com>
2019-07-20 11:52:41 +0200

    Move welcome to its own Component into header section

---
+++ src/components/Header/Welcome/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './style.scss'
+
+function Welcome(props) {
+  return (
+    <div className='welcome-container'>
+      <div className="welcome">Welcome to the street food You love!</div>
+    </div>
+  )
+}
+
+export default Welcome;
