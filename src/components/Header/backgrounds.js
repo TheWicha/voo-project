516062b68b4c79ecd0aeb559fc2167b222bee752
TheWicha <kontakt.danielwisniewski@gmail.com>
2019-07-20 11:57:19 +0200

    Add background-image change over time with fade

---
+++ src/components/Header/backgrounds.js
@@ -0,0 +1,48 @@
+import img1 from '../../images/food-carusele/food1.jpeg'
+import img2 from '../../images/food-carusele/food2.jpeg'
+import img3 from '../../images/food-carusele/food3.jpeg'
+import img4 from '../../images/food-carusele/food4.jpeg'
+import img5 from '../../images/food-carusele/food5.jpeg'
+import img6 from '../../images/food-carusele/food6.jpeg'
+import img7 from '../../images/food-carusele/food7.jpeg'
+import img8 from '../../images/food-carusele/food8.jpeg'
+import img9 from '../../images/food-carusele/food9.jpeg'
+import img10 from '../../images/food-carusele/food10.jpeg'
+import img11 from '../../images/food-carusele/food11.jpeg'
+
+
+export default [
+  {
+    url: img1,
+  },
+  {
+    url: img2,
+  },
+  {
+    url: img3,
+  },
+  {
+    url: img4,
+  },
+  {
+    url: img5,
+  },
+  {
+    url: img6,
+  },
+  {
+    url: img7,
+  },
+  {
+    url: img8,
+  },
+  {
+    url: img9,
+  },
+  {
+    url: img10,
+  },
+  {
+    url: img11,
+  }
+]
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,20 +1,83 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './style.scss'
-
+import Welcome from './Welcome'
 import Navbar from './Navbar';
+import backgrounds from './backgrounds';
 
-function Header(props) {
-  return (
-    <section className='header'>
-      <div className='header-container'>
-        <Navbar />
-        <div className='welcome-container'>
-          <div className="welcome">Welcome to the street food You love!</div>
-        </div>
+class Header extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      currentIteration: 0,
+      currentImg: backgrounds[0].url,
+      imgTwo: backgrounds[0].url,
+    }
+  }
+
+  componentDidMount() {
+    this.caruselInterval = setInterval(() => {
+      const animatedHeader = document.querySelector('.header')
+
+      animatedHeader.classList.remove('one-transition')
+      animatedHeader.classList.add('zero-transition')
+      animatedHeader.style.opacity = 0;
+
+      this.setState((state) => {
+        let { currentIteration } = state
+        if (currentIteration === (backgrounds.length - 1)) {
+          return {
+            currentIteration: 0,
+            currentImg: backgrounds[0].url,
+            imgTwo: state.currentImg
+          }
+
+        }
+        return { currentIteration: currentIteration + 1, currentImg: backgrounds[currentIteration + 1].url, imgTwo: backgrounds[currentIteration].url }
+      })
+    }, 10000);
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.currentIteration !== this.state.currentIteration) {
+      const animatedHeader = document.querySelector('.header')
+
+
+      setTimeout(() => {        
+        animatedHeader.classList.remove('zero-transition') 
+        animatedHeader.classList.add('one-transition')
+        animatedHeader.style.opacity = 1;
+      }, 1000)
+    }
+  }
+  componentWillUnmount() {
+    if (window.scrollY > 583) {
+      clearInterval(this.caruselInterval)
+      return
+    }
+  }
+
+  renderHeaderSection = (currentImg) => {
+
+    return <section className='header' style={{ opacity: 0, backgroundImage: `linear-gradient(to bottom, rgba(22, 22, 22, 4.3) 0, rgba(255, 255, 255, -1) 48.4%), url(${currentImg})` }} />
+  }
+
+  render() {
+    const { currentImg, imgTwo } = this.state
+
+    return (
+      <div className='header-back' style={{ backgroundImage: `linear-gradient(to bottom, rgba(22, 22, 22, 4.3) 0, rgba(255, 255, 255, -1) 48.4%), url(${imgTwo})` }}>
+        {this.renderHeaderSection(currentImg)}
+        <section className='header-welcome'>
+          <div className='header-container'>
+            <Navbar />
+            <Welcome />
+          </div>
+        </section>
       </div>
-
-    </section>
-  )
+    )
+  }
 }
 
 export default Header;
+
