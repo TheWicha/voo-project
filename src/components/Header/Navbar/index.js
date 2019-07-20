fd6fb0c62cd5b462508ef1ea1ac28a1e0f2307e7
TheWicha <kontakt.danielwisniewski@gmail.com>
2019-07-20 11:54:59 +0200

    Add color change to navbar when navbar is moved

--- src/components/Header/Navbar/index.js
+++ src/components/Header/Navbar/index.js
@@ -1,21 +1,44 @@
 import React, { Component } from 'react';
-import id from 'shortid'
 import links from './links'
-import Logo from '../Logo'
+import Logo from './Logo'
 import './style.scss'
 
 
 class Navbar extends Component {
+  constructor(props) {
+    super(props)
 
+    this.state = {
+      isInMove: false,
+    };
+
+    
+    this.handleColorChange = this.handleColorChange.bind(this)
+  };
+
+  componentDidMount(){
+    window.addEventListener('scroll', this.handleColorChange)
+  }
+  componentWillUnmount(){
+    window.addEventListener('scroll', this.handleColorChange)
+  }
+
+  handleColorChange() {
+    if (window.scrollY > 55) {
+      return this.setState({ isInMove: true })
+    }
+    return this.setState({ isInMove: false });
+  }
 
   render() {
+    const { isInMove } = this.state
     return (
-      <div className="navbar-container">
+      <div className={isInMove ? 'navbar-container in-move' : "navbar-container"}>
         <div className='navbar-wrapper'>
           <Logo />
           <nav className="navi">
             {links.map(link => (
-              <li className='link' key={id.generate()}> <a href={link.href}>{link.name}</a> </li>
+              <li className='link' key={link.id}> <a href={link.href}>{link.name}</a> </li>
             ))}
           </nav>
         </div>
--- src/components/Header/Navbar/links.js
+++ src/components/Header/Navbar/links.js
@@ -1,18 +1,24 @@
+import id from 'shortid'
+
 export default [
   {
     name: 'home',
-    href: '#home'
+    href: '#home',
+    id: id.generate(),
   },
   {
     name: 'about us',
-    href: '#about-us'
+    href: '#about-us',
+    id: id.generate(),
   },
   {
     name: 'menu',
-    href: '#menu'
+    href: '#menu',
+    id: id.generate(),
   },
   {
     name: 'locations',
-    href: '#locations'
+    href: '#locations',
+    id: id.generate(),
   },
 ]
