b2bf62b63a5bacaf24f94882e7bb46a11b141635
TheWicha <kontakt.danielwisniewski@gmail.com>
2019-07-24 15:15:32 +0200

    code refactor

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,21 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './style.scss'
 import img from '../../images/voo.png'
 
-function About(props) {
-  return (
-    <section className='about'>
-      <div className='about-us'>
-        <h1>Our Story</h1>
-        <img src={img} alt='Voo logo' />
-        <p>
-          Voo is a brick & mortar, quick service restaurant inspired by street vendors,
-          carts & trucks from across the country & the globe. Check out either of our locations
-          for a quick bite or hang at bar for some of the best nightlife in Poland.
-        </p>
-      </div>
-    </section>
-  )
+class About extends Component {
+
+
+
+  setRef(sectionName) {
+    return this.props.setRef(sectionName)
+  }
+
+  render() {
+
+    return (
+      <section ref={this.setRef('about')} id='about-us' className='about' >
+        <div className='about-us'>
+          <h1>Our Story</h1>
+          <img src={img} alt='Voo logo' />
+          <p>
+            Voo is a brick & mortar, quick service restaurant inspired by street vendors,
+            carts & trucks from across the country & the globe. Check out either of our locations
+            for a quick bite or hang at bar for some of the best nightlife in Poland.
+          </p>
+        </div>
+      </section>
+    )
+  }
 }
 
 export default About;
