9e873471bd3aae2b1af30afb28a683d573ccbbbc
TheWicha <kontakt.danielwisniewski@gmail.com>
2019-07-24 15:12:02 +0200

    Move state to parent component

--- src/components/Header/Navbar/index.js
+++ src/components/Header/Navbar/index.js
@@ -1,53 +1,28 @@
-import React, { Component } from 'react';
-import links from './links'
-import Logo from './Logo'
-import './style.scss'
+import React from 'react';
+import links from './links';
+import Logo from './Logo';
+import './style.scss';
 
 
-class Navbar extends Component {
-  constructor(props) {
-    super(props)
-
-    this.state = {
-      isInMove: false,
-    };
-
+function Navbar(props){
+  const { isInMove, activeIndex } = props
     
-    this.handleColorChange = this.handleColorChange.bind(this)
-  };
-
-  componentDidMount(){
-    window.addEventListener('scroll', this.handleColorChange)
-  }
-  componentWillUnmount(){
-    window.addEventListener('scroll', this.handleColorChange)
-  }
-
-  handleColorChange() {
-    if (window.scrollY > 55) {
-      return this.setState({ isInMove: true })
-    }
-    return this.setState({ isInMove: false });
-  }
-
-  render() {
-    const { isInMove } = this.state
-    return (
-      <div className={isInMove ? 'navbar-container in-move' : "navbar-container"}>
-        <div className='navbar-wrapper'>
-          <Logo />
-          <nav className="navi">
-            {links.map(link => (
-              <li className='link' key={link.id}> <a href={link.href}>{link.name}</a> </li>
-            ))}
-          </nav>
-        </div>
+  return (
+    <div className={isInMove ? 'navbar-container in-move' : "navbar-container"}>
+      <div className='navbar-wrapper'>
+        <Logo />
+        <nav className="navi">
+          {links.map((link, i) => (
+            <li className='link' key={link.id}> 
+              <a className={i === activeIndex ? 'activated' : null} 
+                 href={link.href}>
+                  {link.name}
+              </a> 
+            </li>
+          ))}
+        </nav>
       </div>
-
-    )
-  }
-
-
+    </div>
+  )
 }
-
 export default Navbar;
