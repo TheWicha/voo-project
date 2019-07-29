7417bf70a98bd0b4a1dbd5ec4d140d7da312338d
TheWicha <kontakt.danielwisniewski@gmail.com>
2019-07-30 00:10:44 +0200

    add Add animations for About component and move out text to PageComponent

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,27 +1,31 @@
 import React, { Component } from 'react';
 import './style.scss'
-import img from '../../images/voo.png'
+import PageText from '../PageText';
 
 class About extends Component {
+  constructor(props) {
+    super(props)
 
+
+    this.title = 'Our Story'
+    this.text = <span>Voo is a brick & mortar, quick service restaurant inspired by street vendors, carts & trucks from across the country & the globe. Check out either of our {<a href='#locations'>locations</a>} for a quick bite or hang at bar for some of the best nightlife in Poland.</span>
+  }
 
 
   setRef(sectionName) {
     return this.props.setRef(sectionName)
   }
 
-  render() {
 
+  render() {
+    const { startAnimation } = this.props
     return (
       <section ref={this.setRef('about')} id='about-us' className='about' >
-        <div className='about-us'>
-          <h1>Our Story</h1>
-          <img src={img} alt='Voo logo' />
-          <p>
-            Voo is a brick & mortar, quick service restaurant inspired by street vendors,
-            carts & trucks from across the country & the globe. Check out either of our locations
-            for a quick bite or hang at bar for some of the best nightlife in Poland.
-          </p>
+        <div className={(startAnimation) ? 'about-us slide-bottom' : 'about-us hide'}>
+          <PageText
+            title={this.title}
+            text={this.text}
+          />
         </div>
       </section>
     )
