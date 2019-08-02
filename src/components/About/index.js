ae02c5532045b7c2befa1558c038b6d448d48946
TheWicha <kontakt.danielwisniewski@gmail.com>
2019-08-02 08:57:19 +0200

    Add animtions for about section

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -8,7 +8,11 @@ class About extends Component {
 
 
     this.title = 'Our Story'
-    this.text = <span>Voo is a brick & mortar, quick service restaurant inspired by street vendors, carts & trucks from across the country & the globe. Check out either of our {<a href='#locations'>locations</a>} for a quick bite or hang at bar for some of the best nightlife in Poland.</span>
+    this.text = <span>
+      Voo is a brick & mortar, quick service restaurant inspired by street vendors,
+                      carts & trucks from across the country & the globe. Check out either of our {<a href='#locations'>locations</a>}
+      for a quick bite or hang at bar for some of the best nightlife in Poland.
+                </span>
   }
 
 
@@ -21,7 +25,7 @@ class About extends Component {
     const { startAnimation } = this.props
     return (
       <section ref={this.setRef('about')} id='about-us' className='about' >
-        <div className={(startAnimation) ? 'about-us slide-bottom' : 'about-us hide'}>
+        <div className={(startAnimation) ? 'about-us slide-bottom' : 'about-us'}>
           <PageText
             title={this.title}
             text={this.text}
