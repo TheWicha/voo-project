ae6f2ee07c9c391a75b48eba6de38967e1ffd42b
TheWicha <kontakt.danielwisniewski@gmail.com>
2019-08-02 09:01:02 +0200

    Add new welcome text and menu and location buttons

--- src/components/Header/Welcome/index.js
+++ src/components/Header/Welcome/index.js
@@ -4,7 +4,19 @@ import './style.scss'
 function Welcome(props) {
   return (
     <div className='welcome-container'>
-      <div className="welcome">Welcome to the street food You love!</div>
+      <div className="welcome">
+        <span className='food1'>
+          world's best
+        </span>
+        <span className='food2'>
+          street food
+        </span>
+        <div className='food-btns'>
+          <a href="#menu" className='food-btn'>check menu</a>
+          <a href="#locations" className='food-btn'>see locations</a>
+        </div>
+
+      </div>
     </div>
   )
 }
